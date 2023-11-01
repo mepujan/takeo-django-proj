3c843d34a29dd98e72ee602bb9e9c5307828e268
Pujan Gautam <mepujan10@gmail.com>
2023-10-31 22:27:30 -0400

    added new field due date in model class

--- task/models.py
+++ task/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
     descriptions = models.TextField("Descriptions of the Task")
+    due_date = models.DateField(default=datetime.now())
 
     def __str__(self) -> str:
         return self.title
